d7c519d0a5bc3666ad575fb11535dabd02a086b4
Aurélien Devaux <oss@aureldvx.net>
2024-11-19 15:57:34 +0100

    wip(api): forgot-password workflow

--- apps/api/start/routes.ts
+++ apps/api/start/routes.ts
@@ -6,6 +6,7 @@ const LoginController = () => import('#controllers/auth/login_controller');
 const CheckController = () => import('#controllers/auth/check_controller');
 const LogoutController = () => import('#controllers/auth/logout_controller');
 const RegisterController = () => import('#controllers/auth/register_controller');
+const ForgotPasswordController = () => import('#controllers/auth/forgot_password_controller');
 
 const DeleteAccountController = () => import('#controllers/account/delete_account_controller');
 
@@ -15,6 +16,12 @@ router
 		router.get('check', [CheckController]).as('auth.check');
 		router.post('register', [RegisterController]).as('auth.register');
 		router.post('logout', [LogoutController]).as('auth.logout').use(middleware.auth());
+		router.post('forgot-password', [ForgotPasswordController]).as('auth.password.forgot');
+		router
+			.post('reset-password/:token', () => {
+				console.log('ici');
+			})
+			.as('auth.password.reset');
 	})
 	.prefix('auth');
 
